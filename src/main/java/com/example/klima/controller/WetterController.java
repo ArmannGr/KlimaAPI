2e60dffefc831deadcfe230352c0c016dbae577d
Armann Grewal <grewal.armann15@gmail.com>
2022-05-25 19:44:16 +0200

    WorldEmissionUsage Implementierung

--- src/main/java/com/example/klima/controller/WetterController.java
+++ src/main/java/com/example/klima/controller/WetterController.java
@@ -1,6 +1,7 @@
 package com.example.klima.controller;
 
 import com.example.klima.dao.WetterDao;
+import com.example.klima.dao.WorldEmissionUsageDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,16 +9,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/wetter")
 public class WetterController {
     private final WetterDao wetterDao;
+    private final WorldEmissionUsageDao worldEmissionUsageDao;
 
     @Autowired
-    public WetterController(WetterDao wetterDao) {
+    public WetterController(WetterDao wetterDao, WorldEmissionUsageDao worldEmissionUsageDao) {
         this.wetterDao = wetterDao;
+        this.worldEmissionUsageDao = worldEmissionUsageDao;
     }
 
     @GetMapping("/importToDB")
     public String importToDB(){
         String folder = "C:/Users/robin/OneDrive/Desktop/test5";
        wetterDao.readWetterFolder(folder);
+        return "Success";
+    }
+
+    @GetMapping("/importToDB")
+    public String importToDBWorldEmissionUsage(){
+        String folder = "C:/Users/robin/OneDrive/Desktop/test5";
+        worldEmissionUsageDao.readWorldEmissionUsageFolder(folder);
         return "Success";
     }
 
